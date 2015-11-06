6efa9de27d9f5d78709b235ef5806f3f2602a883
Christian Westhoff <ywesthoffchristian1@gmail.com>
2015-11-06 18:04:55 +0100

    asdd

--- app/consts.js
+++ app/consts.js
@@ -10,4 +10,4 @@ exports.SKYPE_PASSWORD = 'Coins2015';
 
 // MONGODB SETTINGS
 //exports.MONGOLAB_DB = 'mongodb://oscar:0QOtOJx7YBnnPHiKV54V@23.102.28.222:27017/oscar';
-exports.MONGOLAB_DB = 'mongodb://skypebot:coins2015@ds041494.mongolab.com:41494/skypebot';
+exports.MONGOLAB_DB = 'mongodb://user:coins2015@ds041494.mongolab.com:41494/skypebot';
