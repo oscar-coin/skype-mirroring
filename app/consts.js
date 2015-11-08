7256fec10ddc1d626817a1f1620446bf7f035ac0
Christian Westhoff <ywesthoffchristian1@gmail.com>
2015-11-08 16:46:43 +0100

    Readded azure db

--- app/consts.js
+++ app/consts.js
@@ -9,5 +9,5 @@ exports.SKYPE_USERNAME = 'coin.bot';
 exports.SKYPE_PASSWORD = 'Coins2015';
 
 // MONGODB SETTINGS
-//exports.MONGOLAB_DB = 'mongodb://oscar:0QOtOJx7YBnnPHiKV54V@23.102.28.222:27017/oscar';
-exports.MONGOLAB_DB = 'mongodb://user:coins2015@ds041494.mongolab.com:41494/skypebot';
+exports.MONGOLAB_DB = 'mongodb://skype:0QOtOJx7YBnnPHiKV54V@meanstack-1842.cloudapp.net:27017/skype';
+//exports.MONGOLAB_DB = 'mongodb://user:coins2015@ds041494.mongolab.com:41494/skypebot';
